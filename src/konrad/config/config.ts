2e30c51f026e5c6ac5132eefa83b86569651ccbb
Paweł Trojanowski <lewapoel@gmail.com>
2022-02-11 15:16:57 +0100

    .

---
+++ src/konrad/config/config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  facebook_api_key: "1439979303070884",
+  facebook_api_secret: "756abe746115c28838b7a4bc71d5e056",
+  google_client_id:
+    "859117807079-f5488an3ftfkcskeg3spa9onl676u9sp.apps.googleusercontent.com",
+  google_client_secret: "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",
+  facebook_callback_url: "http://localhost:3000/auth/facebook/callback",
+  google_callback_url: "http://localhost:3000/auth/google/callback",
+  use_database: false,
+  host: "localhost",
+  username: "",
+  password: "",
+  database: "",
+  jwtSecret: "$k!tr$j2022",
+};
---
+++ src/konrad/routes/router.ts
@@ -0,0 +1,190 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
+
+// api reference
+const apiReference = `<pre>
+GET:
+
+  all users:
+    url: /users
+
+  user by id: /users/:id
+
+  facebook (register,login):
+    url: /auth/facebook
+
+  google (register,login):
+    url: /auth/google
+
+POST:
+
+  create user normal password:
+    url: /users
+    body: {'email': 'normal@gmail.com', password: '12345'}
+
+  login normal password:
+    url: /auth/normal-password
+    body: {'email': 'normal@gmail.com', password: '12345'}
+
+PUT:
+
+  update user:
+    url: /user/:id
+    body: {'firstName': 'John'}
+
+DELETE:
+
+  delete user by id:
+    url: /users/:id
+
+  <pre>`;
+
+// homepage
+router.get("/", (req, res) => {
+  res.send(apiReference);
+});
+
+// get all users
+router.get("/users", async (req, res) => {
+  const userRepository = getRepository(User);
+  res.send(JSON.stringify(await userRepository.find()));
+});
+
+// get particular user by id
+router.get("/users/:userId", async (req, res) => {
+  const userRepository = getRepository(User);
+  res.send(JSON.stringify(await userRepository.findOne(req.params.userId)));
+});
+
+// create new user
+router.post("/users", async (req, res) => {
+  const userRepository = getRepository(User);
+  const { email } = req.body;
+  const user = new User();
+
+  user.email = email;
+
+  try {
+    await userRepository.save(user);
+    res.send("User created");
+  } catch (e) {
+    res.send("E-mail already in use");
+    return;
+  }
+});
+
+// login user using normal password
+router.post("/auth/normal-password", async (req, res) => {
+  const userRepository = getRepository(User);
+
+  const user: User = await userRepository.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (user !== undefined) {
+    // user authenticated, log in him
+    res.send("Logging in...");
+  } else {
+    // user does not exist, access denied
+    res.send("Login denied...");
+  }
+});
+
+// update particular user
+router.put("/users/:userId", async (req, res) => {
+  const userRepository = getRepository(User);
+  res.send(
+    JSON.stringify(
+      await userRepository.update({ id: req.params.userId }, req.body)
+    )
+  );
+});
+
+// delete particular user
+router.delete("/users/:userId", async (req, res) => {
+  const userRepository = getRepository(User);
+  res.send(JSON.stringify(await userRepository.delete(req.params.userId)));
+});
+
+// order email and profile from google auth
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// callback function from google
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    // user successfully authenticated
+    successRedirect: "/success-google",
+    // can't authenticate
+    failureRedirect: "/error",
+  })
+);
+
+// prints successfully authenticated user in a browser
+router.get("/success-google", async (req, res) => {
+  const userRepository = getRepository(User);
+
+  const user: User = await userRepository.findOne({
+    email: req.user.emails[0].value,
+  });
+
+  if (user !== undefined) {
+    // user already created, we can log in him
+    res.send("Logging in...");
+  } else {
+    // user does not exist, create him and log in afterwards
+    const user = new User();
+    user.email = req.user.emails[0].value;
+    await userRepository.save(user);
+    res.send("I had to create a user first. Logging in...");
+  }
+});
+
+// order email and profile from facebook
+router.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: "email" })
+);
+
+// callback function from facebook
+router.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    // user successfully authenticated
+    successRedirect: "/success-facebook",
+    // can't authenticate
+    failureRedirect: "/error",
+  })
+);
+
+// prints successfully authenticated user in a browser
+router.get("/success-facebook", async (req, res) => {
+  const userRepository = getRepository(User);
+
+  const user: User = await userRepository.findOne({
+    email: req.user.emails[0].value,
+  });
+
+  if (user !== undefined) {
+    // user already created, we can log in him
+    res.send("Logging in...");
+  } else {
+    // user does not exist, create him and log in afterwards
+    const user = new User();
+    user.email = req.user.emails[0].value;
+    await userRepository.save(user);
+    res.send("I had to create a user first. Logging in...");
+  }
+});
+
+// prints error message in a browser, in case of authentication error
+router.get("/error", (req, res) => res.send("error logging in"));
+
+module.exports = router;
